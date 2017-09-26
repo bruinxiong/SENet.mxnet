a52ff49ea6e3c7ab8f50ac142b9befffca98888f
Xiong Lin <bruinxiongmac@gmail.com>
2017-09-26 21:05:02 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+export MXNET_CUDNN_AUTOTUNE_DEFAULT=1
+
+# you'd better change setting with your own --data-dir, --depth, --batch-size, --gpus.
+
+## train se-resnext-50
+python -u train_se_resnext_w_d.py --data-dir data/imagenet --data-type imagenet --depth 50 --batch-size 192 --num-group 64 --drop-out 0.0 --gpus=6,7,8,9
+
+## train se-resnext-101
+#python -u train_se_resnext_w_d.py --data-dir data/imagenet --data-type imagenet --depth 101 --batch-size 128 --num-group 64 --drop-out 0.0 --gpus=2,3,4,5
