60d3c5672c99164203b121156e07f3dc89c4e515
Xiong Lin <bruinxiongmac@gmail.com>
2017-09-27 15:11:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,3 +8,9 @@ python -u train_se_resnext_w_d.py --data-dir data/imagenet --data-type imagenet 
 
 ## train se-resnext-101
 #python -u train_se_resnext_w_d.py --data-dir data/imagenet --data-type imagenet --depth 101 --batch-size 128 --num-group 64 --drop-out 0.0 --gpus=2,3,4,5
+
+## train se-resnet-50
+#python -u train_se_resnet.py --data-dir data/imagenet --data-type imagenet --depth 50 --batch-size 256 --gpus=6,7,8,9
+
+## train se-resnet-101
+#python -u train_se_resnet.py --data-dir data/imagenet --data-type imagenet --depth 101 --batch-size 256 --gpus=2,3,4,5
